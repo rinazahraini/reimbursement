eff6077b5b72704e6030d9a5e01be41184090cfe
rina <rinazahrotulaini@gmail.com>
2021-12-15 14:28:51 +0700

    package jenssegers date

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          */
         
         Yudi1212\AutoNumber\AutoNumberServiceProvider::class,
+        Jenssegers\Date\DateServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -231,7 +232,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Date' => Jenssegers\Date\Date::class,
+        
     ],
 
 ];
